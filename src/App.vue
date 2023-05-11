bf883992054a7569fd1d18429d1a49a43c593e96
Melike Altın <melikealtin414@gmail.com>
2023-05-12 01:15:53 +0300

    Create .env

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,8 @@
 export default {
   data() {
     return {
-      api_key: "dc22a3f0ff61412e89978c67cb09006e",
-      url_base: "https://api.openweathermap.org/data/2.5/",
+      api_key: import.meta.env.VITE_WEATHER_API_KEY,
+      url_base: import.meta.env.VITE_WEATHER_URL_BASE,
       query: "",
       weather: {},
     };
@@ -46,7 +46,7 @@ export default {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
-          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+          `${this.url_base}/weather?q=${this.query}&units=metric&APPID=${this.api_key}`
         )
           .then((res) => {
             return res.json();
@@ -90,6 +90,9 @@ export default {
 
       return `${day} ${date} ${month} ${year}`;
     },
+  },
+  mounted() {
+    console.log(import.meta.env);
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// import "dotenv/config";
 
 createApp(App).mount("#app");
